928e1dcc448f2867b947c7cf8e78e3f8afe7ff99
Creak1 <zsokol20@gmail.com>
2024-07-06 12:19:58 +0300

    add slick slider for phone

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-$('.friends').slick();
+$(document).ready(function(){
+    $('.your-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    });
+    $('.friends-slider').slick({
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    });
+});
